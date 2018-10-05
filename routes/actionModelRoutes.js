292704dee29634a266685042af6b3c549d33c0a2
Michael Agard <mdagard@gmail.com>
2018-10-05 13:32:42 -0400

    added some error handling but not able to read if id exists

--- routes/actionModelRoutes.js
+++ routes/actionModelRoutes.js
@@ -5,26 +5,27 @@ const actionModel = require('../data/helpers/actionModel');
 router.get('/', (req, res) => {
   actionModel.get()
     .then(actionModel => {
-      if (!actionModel) { return res.json({ message: `no user by that id` }) }
       res.json(actionModel);
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }) // return all actions
 
 router.get('/:id', (req, res) => {
   actionModel.get(req.params.id)
     .then(actionModel => {
-      if (!actionModel) { return res.json({ message: `no actionModel by id: ${req.params.id}` }) }
+      // if (actionModel.id === undefined) { return res.json({ message: `no actionModel by id: ${req.params.id}` }) }
+      // the above doesn't work due to the error recieved back "Cannot read property 'completed' of undefined"
+      // not sure how to proceed
       res.json(actionModel);
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }); // supply actionModel ID, recieve actionModel object
 
 
 router.post('/', (req, res) => {
   actionModel.insert(req.body)
     .then(() => res.json({ message: `Success!` }))
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }); // add action, return action
 
 router.put('/:id', (req, res) => {
@@ -32,7 +33,7 @@ router.put('/:id', (req, res) => {
     .then(() => {
       res.json({ message: `Success!` })
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }); // update action, recieve updated action
 
 router.delete('/:id', (req, res) => {
@@ -40,6 +41,6 @@ router.delete('/:id', (req, res) => {
     .then(removed => {
       res.json({ message: `Success: ${req.params.id} was deleted. Removed ${removed} action.` })
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }) // delete action, recieve deleted count
 module.exports = router;
--- routes/projectModelRoutes.js
+++ routes/projectModelRoutes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     .then(projectModel => {
       res.json(projectModel);
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }) // return all projects
 
 router.get('/:id', (req, res) => {
@@ -16,13 +16,13 @@ router.get('/:id', (req, res) => {
       if (!projectModel) { return res.json({ message: `no projectModel by that id` }) }
       res.json(projectModel);
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }); // supply projectModel ID, recieve projectModel object
 
 router.post('/', (req, res) => {
   projectModel.insert(req.body)
     .then(() => res.json({ message: `Success!` }))
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }); // add project, return project
 
 router.put('/:id', (req, res) => {
@@ -30,7 +30,7 @@ router.put('/:id', (req, res) => {
     .then(() => {
       res.json({ message: `Success!` })
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }); // update project, recieve updated project
 
 router.delete('/:id', (req, res) => {
@@ -38,14 +38,14 @@ router.delete('/:id', (req, res) => {
     .then(removedProjectCount => {
       res.json({ message: `Success: ${req.params.id} was deleted. Removed ${removedProjectCount} action.` })
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 })
 router.get('/projectactions/:id', (req, res) => {
   projectModel.getProjectActions(req.params.id)
     .then(project => {
       res.json(project);
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log({ Message: `Error: ${err}` }));
 }); // supply project ID, recieve project actions
 
 module.exports = router;
